48ce6328e6358886ca2325be946bfe9f1d5c4986
Manuel <88690070+jmig21@users.noreply.github.com>
2023-10-29 15:01:21 -0300

    "add login & signin components"

--- ProyectoFinal/src/app/app-routing.module.ts
+++ ProyectoFinal/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from "./app.component";
 
-const routes: Routes = [];
+import { LoginComponent } from "./login/login.component";
+import { SigninComponent } from "./signin/signin.component"
+
+const routes: Routes = [
+  { path: "", component: AppComponent, pathMatch: "full" },
+  { path: "login", component: LoginComponent, pathMatch: "full" },
+  { path: "signin", component: SigninComponent, pathMatch: "full" },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
