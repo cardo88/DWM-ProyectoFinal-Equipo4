7c412ebda9435a430d393246267487fc2fcb8b03
cardo88 <cardo88@gmail.com>
2023-11-01 20:00:22 -0300

    RC - previo a merge de lucas(dev)

--- src/app/components/player-join/player-join.component.ts
+++ src/app/components/player-join/player-join.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-player-join',
@@ -16,12 +17,40 @@ export class PlayerJoinComponent {
     // private toastr: ToastrService,
     // private _productService: ProductService,
     // private aRouter: ActivatedRoute
-    ) {
+  ) {
     this.joinForm = this.fb.group({
       nickname: ['', Validators.required],
-      code: ['', Validators.required],
+      code: ['', [Validators.required,this.validarLongitudCodigo]],
     })
   }
 
-  addPlayer(){}
+  validarLongitudCodigo(control:any) {
+    if (control.value && control.value.length !== 4) {
+      return { 'longitudIncorrecta': true };
+    }
+    return null;
+  }
+
+
+
+  player_nickname: string = "";
+  player_code: string = "";
+  room_code: string = "4444"; //codigo ejemplo para aceptar la conexión
+
+  //permite en CODE solo ingresar un codigo de hasta 4 digitos.
+  onlyNumbers(event: KeyboardEvent) {
+    const pattern = /^[0-9]*$/;
+    const inputChar = event.key;
+
+    if (!pattern.test(inputChar) || this.player_code.length >= 4) {
+      event.preventDefault();
+    }
+  }
+
+
+  joinPlayer() {
+
+  }
+
+
 }
